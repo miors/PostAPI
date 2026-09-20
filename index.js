53e0516290af0af918342de90b8835528ae66872
Sigmaschool <152258968+teamsigmaschool@users.noreply.github.com>
2026-09-20 22:37:18 +0800

    Update file
    Added middleware to parse JSON requests.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
