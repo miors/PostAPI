ed4d457b0da63f791d749264aae43e3477180eae
Mior Sufian <mior.sufian@googlemail.com>
2026-09-22 14:25:28 +0800

    modded files

--- index.js
+++ index.js
@@ -1,12 +1,108 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
+const { Pool } = require("pg");
+require('dotenv').config({ path: './.env' });
+// require('dotenv').config()
+
+// console.log("Hello",process.env.DATABASE_URL)
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
+app.get("/db-check", async (req, res) => {
+  // console.log("Hello",process.env.DATABASE_URL)
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows[0]);
+});
+
+app.get("/posts", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM posts");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.post("/posts", async (req, res) => {
+  const { title, content, author } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO posts (title, content, author) VALUES ($1, $2, $3) RETURNING *",
+      [title, content, author],
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.patch("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, content, author } = req.body;
+  try {
+    const result = await pool.query(
+      "UPDATE posts SET title = $1, content = $2, author = $3 WHERE id = $4 RETURNING *",
+      [title, content, author, id],
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.delete("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      "DELETE FROM posts WHERE id = $1 RETURNING *",
+      [id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    res.json({ message: "Post deleted", post: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+
+
+app.listen(3004, () => {
   console.log("App is listening on port 3000");
 });
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ To run the application open the terminal and write
 
 1. `npm i`
 2. `npm run dev`
+# PostAPI
